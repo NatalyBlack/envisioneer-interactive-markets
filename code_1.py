d7cf0d1fdcd769baede15dc527c2d175e6182caf
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-10-01 07:14:41 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,43 +1,52 @@
 
-main();
-    const data = generateRandomData();
-}
-function main() {
-    });
-    }
-    const data = [];
 
-    return data;
-}
-    for (let i = 0; i < 10; i++) {
-
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-function generateRandomData() {
-    data.forEach(item => {    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-
-    main()
-
-def generate_random_data():
-    return data
-    data = generate_random_data()
-def main():
 import random
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    return data
+    main()
+    data = generate_random_data()
+def generate_random_data():
+def main():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+def main():
+    main()
+import random
+if __name__ == "__main__":
         print(f"Random Number: {item}")
     for item in data:
 
-        print(f"Random Number: {item}")
-
-
-    data = generate_random_data()
-    main()
-    return data
-if __name__ == "__main__":
 
     data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-def main():
+    data = generate_random_data()
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
 
 def generate_random_data():
+
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+    main()
+def main():
 import random
+if __name__ == "__main__":
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+
+    public static void main(String[] args) {
+    }
+        for (int item : data) {
+        }
+public class RandomDataGenerator {
+}
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
